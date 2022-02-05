8b48d12c101a5453eb5fdc347d56466b4800b24e
Mouse7276 <jangjunhee1@snu.ac.kr>
2022-02-05 13:25:47 +0900

    2022.02.05

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,144 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  static const String _title = 'HomeScreen';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          textTheme: Theme.of(context).textTheme.apply(
-                bodyColor: Colors.brown,
-                displayColor: Colors.white,
-              )),
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
+        scrollBehavior: AppScrollBehavior(), title: _title, home: HomeScreen());
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+final PageController pageController = PageController(
+  initialPage: 0,
+);
 
+class AppScrollBehavior extends MaterialScrollBehavior {
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Rainbow'),
+        title: Text('Pages'),
       ),
-      body: ListWheelScrollView(
-        children: [
-          Container(
-            color: Colors.red,
-            height: 300,
-          ),
-          Container(
-            color: Colors.orange,
-            height: 300,
-          ),
-          Container(
-            color: Colors.yellow,
-            height: 300,
-          ),
-          Container(
-            color: Colors.green,
-            height: 300,
-          ),
-          Container(
-            color: Colors.blue,
-            height: 300,
-          ),
-          Container(
-            color: Colors.cyan,
-            height: 300,
-          ),
-          Container(
-            color: Colors.purple,
-            height: 300,
-          ),
+      body: PageView(
+        controller: pageController,
+        children: <Widget>[
+          HomeScreen1(),
+          HomeScreen2(),
+          HomeScreen1(),
         ],
-        itemExtent: 200,
       ),
     );
+  }
+}
+
+class HomeScreen1 extends StatefulWidget {
+  const HomeScreen1({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreen1State createState() => _HomeScreen1State();
+}
+
+class _HomeScreen1State extends State<HomeScreen1> {
+  var _boolfirst = false;
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        CheckboxListTile(
+          value: _boolfirst,
+          title: Text('First Checkbox'),
+          subtitle: Text('this is first checkbox'),
+          activeColor: Colors.green,
+          checkColor: Colors.black,
+          controlAffinity: ListTileControlAffinity.platform,
+          secondary: Container(
+            height: 100,
+            child: Icon(Icons.hourglass_empty),
+          ),
+          onChanged: (value) {
+            setState(() {
+              _boolfirst = value!;
+            });
+          },
+        ),
+        Container(
+          height: 300,
+          color: Colors.amber[600],
+          child: const Center(child: Text('A')),
+        ),
+        Container(
+          height: 300,
+          color: Colors.amber[500],
+          child: const Center(child: Text('B')),
+        ),
+        Container(
+          height: 300,
+          color: Colors.amber[400],
+          child: const Center(child: Text('C')),
+        ),
+        Container(
+          height: 300,
+          color: Colors.amber[300],
+          child: const Center(child: Text('D')),
+        ),
+        Container(
+          height: 300,
+          color: Colors.amber[200],
+          child: const Center(child: Text('E')),
+        ),
+        Container(
+          height: 300,
+          color: Colors.amber[100],
+          child: const Center(child: Text('F')),
+        ),
+      ],
+    );
+  }
+}
+
+class HomeScreen2 extends StatefulWidget {
+  const HomeScreen2({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreen2State createState() => _HomeScreen2State();
+}
+
+class _HomeScreen2State extends State<HomeScreen2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class HomeScreen3 extends StatefulWidget {
+  const HomeScreen3({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreen3State createState() => _HomeScreen3State();
+}
+
+class _HomeScreen3State extends State<HomeScreen3> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }

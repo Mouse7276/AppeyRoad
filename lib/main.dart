3e3d36c281bda87526474dff51fa42c7884b7d5b
Mouse7276 <jangjunhee1@snu.ac.kr>
2022-02-09 13:29:27 +0900

    2022.02.09

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        scrollBehavior: AppScrollBehavior(), title: _title, home: HomeScreen());
+      scrollBehavior: AppScrollBehavior(),
+      title: _title,
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
@@ -34,15 +38,12 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pages'),
-      ),
       body: PageView(
         controller: pageController,
         children: <Widget>[
           HomeScreen1(),
           HomeScreen2(),
-          HomeScreen1(),
+          HomeScreen3(),
         ],
       ),
     );
@@ -60,57 +61,60 @@ class _HomeScreen1State extends State<HomeScreen1> {
   var _boolfirst = false;
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.all(8),
-      children: <Widget>[
-        CheckboxListTile(
-          value: _boolfirst,
-          title: Text('First Checkbox'),
-          subtitle: Text('this is first checkbox'),
-          activeColor: Colors.green,
-          checkColor: Colors.black,
-          controlAffinity: ListTileControlAffinity.platform,
-          secondary: Container(
-            height: 100,
-            child: Icon(Icons.hourglass_empty),
+    return Scrollbar(
+      isAlwaysShown: true,
+      child: ListView(
+        padding: const EdgeInsets.all(8),
+        children: <Widget>[
+          CheckboxListTile(
+            value: _boolfirst,
+            title: Text('First Checkbox'),
+            subtitle: Text('this is first checkbox'),
+            activeColor: Colors.green,
+            checkColor: Colors.black,
+            controlAffinity: ListTileControlAffinity.platform,
+            secondary: Container(
+              height: 100,
+              child: Icon(Icons.hourglass_empty),
+            ),
+            onChanged: (value) {
+              setState(() {
+                _boolfirst = value!;
+              });
+            },
           ),
-          onChanged: (value) {
-            setState(() {
-              _boolfirst = value!;
-            });
-          },
-        ),
-        Container(
-          height: 300,
-          color: Colors.amber[600],
-          child: const Center(child: Text('A')),
-        ),
-        Container(
-          height: 300,
-          color: Colors.amber[500],
-          child: const Center(child: Text('B')),
-        ),
-        Container(
-          height: 300,
-          color: Colors.amber[400],
-          child: const Center(child: Text('C')),
-        ),
-        Container(
-          height: 300,
-          color: Colors.amber[300],
-          child: const Center(child: Text('D')),
-        ),
-        Container(
-          height: 300,
-          color: Colors.amber[200],
-          child: const Center(child: Text('E')),
-        ),
-        Container(
-          height: 300,
-          color: Colors.amber[100],
-          child: const Center(child: Text('F')),
-        ),
-      ],
+          Container(
+            height: 300,
+            color: Colors.amber[600],
+            child: const Center(child: Text('A')),
+          ),
+          Container(
+            height: 300,
+            color: Colors.amber[500],
+            child: const Center(child: Text('B')),
+          ),
+          Container(
+            height: 300,
+            color: Colors.amber[400],
+            child: const Center(child: Text('C')),
+          ),
+          Container(
+            height: 300,
+            color: Colors.amber[300],
+            child: const Center(child: Text('D')),
+          ),
+          Container(
+            height: 300,
+            color: Colors.amber[200],
+            child: const Center(child: Text('E')),
+          ),
+          Container(
+            height: 300,
+            color: Colors.amber[100],
+            child: const Center(child: Text('F')),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -125,8 +129,49 @@ class HomeScreen2 extends StatefulWidget {
 class _HomeScreen2State extends State<HomeScreen2> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scrollbar(
+      isAlwaysShown: true,
+      child: GridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 15,
+        crossAxisSpacing: 15,
+        children: [
+          Container(
+            height: 60,
+            color: Colors.amber[600],
+            child: const Center(child: Text('A')),
+          ),
+          Container(
+            height: 60,
+            color: Colors.amber[500],
+            child: const Center(child: Text('B')),
+          ),
+          Container(
+            height: 60,
+            color: Colors.amber[400],
+            child: const Center(child: Text('C')),
+          ),
+          Container(
+            height: 60,
+            color: Colors.amber[300],
+            child: const Center(child: Text('D')),
+          ),
+          Container(
+            height: 60,
+            color: Colors.amber[200],
+            child: const Center(child: Text('E')),
+          ),
+          Container(
+            height: 60,
+            color: Colors.amber[100],
+            child: const Center(child: Text('F')),
+          ),
+        ],
+      ),
+    );
   }
+
+  launch(String s) {}
 }
 
 class HomeScreen3 extends StatefulWidget {
@@ -139,6 +184,62 @@ class HomeScreen3 extends StatefulWidget {
 class _HomeScreen3State extends State<HomeScreen3> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          pinned: true,
+          expandedHeight: 150,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text("New Appbar", style: TextStyle(color: Colors.black)),
+            background: Image.asset(
+              'images/background.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SliverFillRemaining(
+          child: Scrollbar(
+            isAlwaysShown: true,
+            child: GridView.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 15,
+              crossAxisSpacing: 15,
+              children: [
+                Container(
+                  height: 60,
+                  color: Colors.amber[600],
+                  child: const Center(child: Text('A')),
+                ),
+                Container(
+                  height: 60,
+                  color: Colors.amber[500],
+                  child: const Center(child: Text('B')),
+                ),
+                Container(
+                  height: 60,
+                  color: Colors.amber[400],
+                  child: const Center(child: Text('C')),
+                ),
+                Container(
+                  height: 60,
+                  color: Colors.amber[300],
+                  child: const Center(child: Text('D')),
+                ),
+                Container(
+                  height: 60,
+                  color: Colors.amber[200],
+                  child: const Center(child: Text('E')),
+                ),
+                Container(
+                  height: 60,
+                  color: Colors.amber[100],
+                  child: const Center(child: Text('F')),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
